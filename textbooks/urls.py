6dbf41aee5e279cca7d3f2b4b4519f41ebf3ca9e
Syndacks <math.milliken@gmail.com>
2017-09-20 17:51:44 -0400

    Add links to the texbtook index page and add textbook_detail view

--- textbooks/urls.py
+++ textbooks/urls.py
@@ -5,4 +5,8 @@ app_name = 'textbooks'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'about', views.about, name='about'),
+    # url(r'about', views.about, name='about'),
+
+    # ex: /polls/5/
+    url(r'^(?P<isbn>[0-9]+)/$', views.textbook_detail, name='textbook_detail'),
 ]
--- textbooks/views.py
+++ textbooks/views.py
@@ -3,11 +3,18 @@ from django.http import HttpResponse
 
 from .models import Textbook
 
-
-def about(request):
-    return render(request, 'textbooks/about.html')
-
-
+# Show all textbooks in the database
 def index(request):
     textbooks = Textbook.objects.all()
     return render(request, 'textbooks/index.html', {'textbooks': textbooks})
+
+
+# Show a single textbook by ISBN
+def textbook_detail(request, isbn):
+    textbook = Textbook.objects.get(isbn=isbn)
+    return render(request, 'textbooks/textbook_detail.html', {'textbook': textbook})
+
+
+# A static "About Me" page
+def about(request):
+    return render(request, 'textbooks/about.html')
